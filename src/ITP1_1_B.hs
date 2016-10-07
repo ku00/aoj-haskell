82980d6ed9c85c794c2eafae89d6580a628116af
ku00 <skentaro36@gmail.com>
2016-10-07 23:57:51 +0900

    solve these:
    - ITP1_1_B
    - ITP1_1_C

--- src/ITP1_1_B.hs
+++ src/ITP1_1_B.hs
@@ -1,9 +1,12 @@
 -- http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_B
 -- X Cubic
 
+-- "3"
+-- => "27"
+
 main = do
     x <- getLine
-    putStrLn $ powerThree $ read x :: Int
+    print $ powerThree $ read x
 
-powerThree:: Int -> Int
+powerThree :: Int -> Int
 powerThree = (^3)
---
+++ src/ITP1_1_C.hs
@@ -0,0 +1,18 @@
+-- http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_C
+-- Rectangle
+
+-- "3 5"
+-- => "15 16"
+
+main = do
+    l <- getLine
+    let (a:b:[]) = map read $ words l :: [Int]
+        x = area a b
+        y = circumference a b
+    putStrLn $ show x ++ " " ++ show y
+
+area :: Int -> Int -> Int
+area a b = a * b
+
+circumference :: Int -> Int -> Int
+circumference a b = (a + b) * 2
