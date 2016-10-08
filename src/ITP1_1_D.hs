120617b032e73e651dc66e649758ec9700d2dc1d
ku00 <skentaro36@gmail.com>
2016-10-08 13:24:03 +0900

    solve ITP1_1_D

---
+++ src/ITP1_1_D.hs
@@ -0,0 +1,15 @@
+-- http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_D
+-- Watch
+
+-- "46979"
+-- => "13:2:59"
+
+main = do
+    s <- getLine
+    putStrLn $ toTime (read s :: Int) 2
+
+toTime :: Int -> Int -> String
+toTime s 0 = show s
+toTime s n = show divSecond ++ ":" ++ toTime modSecond (n-1)
+    where divSecond  = s `div` (60 ^ n)
+          modSecond  = s `mod` (60 ^ n)
