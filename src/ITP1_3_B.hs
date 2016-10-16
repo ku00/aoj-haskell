e937b52557b9fbe366d88a63b46d4e4c9a12d862
ku00 <skentaro36@gmail.com>
2016-10-16 17:12:17 +0900

    solve ITP1_3_B

---
+++ src/ITP1_3_B.hs
@@ -0,0 +1,30 @@
+-- http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_3_B
+-- Print Test Cases
+
+-- input:
+-- 3
+-- 5
+-- 11
+-- 7
+-- 8
+-- 19
+-- 0
+
+-- output:
+-- Case 1: 3
+-- Case 2: 5
+-- Case 3: 11
+-- Case 4: 7
+-- Case 5: 8
+-- Case 6: 19
+
+import qualified Control.Monad as Monad
+
+main = loopPutCase 1
+
+loopPutCase :: Int -> IO ()
+loopPutCase i = do
+    x <- getLine
+    Monad.when (read x /= 0) $ do
+        putStrLn $ "Case " ++ show i ++ ": " ++ x
+        loop $ i + 1
