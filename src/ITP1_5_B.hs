1c24fa97262803d4f7fd7e2fe1542b46b83c5c8d
ku00 <skentaro36@gmail.com>
2016-10-21 00:12:18 +0900

    wip ITP1_5_B
    - input: "1 1" output: "##"

---
+++ src/ITP1_5_B.hs
@@ -0,0 +1,54 @@
+-- http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_5_B
+-- Print a Frame
+
+-- input:
+-- 3 4
+-- 5 6
+-- 3 3
+-- 2 2
+-- 1 1
+-- 0 0
+
+-- output:
+-- ####
+-- #..#
+-- ####
+--
+-- ######
+-- #....#
+-- #....#
+-- #....#
+-- ######
+--
+-- ###
+-- #.#
+-- ###
+--
+-- ##
+-- ##
+--
+-- #
+--
+
+import Control.Applicative
+import qualified Control.Monad as Monad
+
+main = loop
+
+loop :: IO ()
+loop = do
+    [h,w] <- map (read :: String -> Int) . words <$> getLine
+    Monad.when (h /= 0 || w /= 0) $ do
+        printFrame h w
+        loop
+
+printFrame :: Int -> Int -> IO ()
+printFrame h w = do
+    Monad.forM_ [1..h] $ \i -> do
+        putStrLn $ "#" ++ frameOrDot h w i ++ "#"
+    putStrLn ""
+
+frameOrDot :: Int -> Int -> Int -> String
+frameOrDot h w i = if (i == 1 || i == h)
+                     then concat . take (w-2) $ repeat "#"
+                     else concat . take (w-2) $ repeat "."
