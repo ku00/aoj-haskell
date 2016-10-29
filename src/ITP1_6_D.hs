663341f39810783357ec3ae17680b6c05aaddbb1
ku00 <skentaro36@gmail.com>
2016-10-29 18:02:45 +0900

    solve ITP1_6_D

---
+++ src/ITP1_6_D.hs
@@ -0,0 +1,43 @@
+-- http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_6_D
+-- Matrix Vector Multiplication
+
+-- input:
+-- 3 4
+-- 1 2 0 1
+-- 0 3 0 1
+-- 4 1 1 0
+-- 1
+-- 2
+-- 3
+-- 0
+
+-- output:
+-- 5
+-- 6
+-- 9
+
+import Control.Applicative
+import qualified Control.Monad as Monad (forM, forM_, replicateM)
+
+type Vector = [Int]
+type Row = [Int]
+type Matrix = [Row]
+
+main = do
+    [n,m] <- getLine'
+    a <- Monad.replicateM n getLine'
+    b <- toVector <$> Monad.replicateM m getLine'
+    mapM_ print $ vectorAndMatrixProduct a b
+
+getLine' :: IO [Int]
+getLine' = map (read :: String -> Int) . words <$> getLine
+
+toVector :: [[Int]] -> Vector
+toVector = concat
+
+vectorAndMatrixProduct :: Matrix -> Vector -> Vector
+vectorAndMatrixProduct xs ys = toVector $ do
+    Monad.forM [0..(length xs - 1)] $ \i -> dotProduct (xs !! i) ys
+
+dotProduct :: Row -> Vector -> Row
+dotProduct xs ys = return $ sum $ zipWith (*) xs ys
