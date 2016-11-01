66bdce30db9e2aeb258872fbea3182627b55066c
ku00 <skentaro36@gmail.com>
2016-11-01 14:21:52 +0900

    solve ITP1_7_D

--- src/ITP1_7_D.hs
+++ src/ITP1_7_D.hs
@@ -14,11 +14,29 @@
 -- 0 9 6
 -- 4 23 14
 
-import Control.Applicative
-import Control.Monad
+import Control.Applicative ((<$>), (<*>))
+import qualified Control.Monad as Monad (replicateM)
+import qualified Data.List as List (transpose)
+import qualified Data.List.Split as Split (chunksOf)
 
 type Row = [Int]
 type Matrix = [Row]
+
+main = do
+    [n,m,l] <- getLine'
+    a <- Monad.replicateM n getLine'
+    b <- Monad.replicateM m getLine'
+    let result = toMatrix l $ dotProduct <$> a <*> List.transpose b
+    mapM_ (putStrLn . printFormat) result
+
+getLine' :: IO [Int]
+getLine' = map (read :: String -> Int) . words <$> getLine
+
+printFormat :: Row -> String
+printFormat xs = unwords $ show <$> xs
+
+toMatrix :: Int -> [Int] -> Matrix
+toMatrix n xs = Split.chunksOf n xs
 
 dotProduct :: Row -> Row -> Int
 dotProduct xs ys = sum $ zipWith (*) xs ys
