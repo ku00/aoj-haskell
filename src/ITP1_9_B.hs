a069ecd009fcbc74a39fad0aeb0e136b9ab71a49
ku00 <skentaro36@gmail.com>
2016-11-10 10:10:03 +0900

    solve ITP1_9_B

--- src/ITP1_9_B.hs
+++ src/ITP1_9_B.hs
@@ -17,6 +17,21 @@
 -- aabc
 -- xyzvw
 
+import Control.Applicative ((<$>))
+import qualified Control.Monad as Monad (when, replicateM)
+
+main = do
+    s <- getLine
+    Monad.when (s /= "-") $ do
+        m <- read <$> getLine
+        hs <- map read <$> Monad.replicateM m getLine
+        putStrLn $ repeatShuffle hs s
+        main
+
+repeatShuffle :: [Int] -> String -> String
+repeatShuffle [] s     = s
+repeatShuffle (x:xs) s = repeatShuffle xs $ shuffle x s
+
 shuffle :: Int -> String -> String
 shuffle h s = s2 ++ s1
     where (s1,s2) = splitAt h s
